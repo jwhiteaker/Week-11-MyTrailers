90a63230eed17ef821bb3d5fd60ca3975d847540
Joe Whiteaker <joe.whiteaker@gmail.com>
2013-04-14 19:31:54 -0500

    Submit to github
    My Movies App homework

---
+++ MyMovies/trailer3ViewController.h
@@ -0,0 +1,17 @@
+//
+//  trailer3ViewController.h
+//  MyMovies
+//
+//  Created by Joseph Whiteaker on 4/14/13.
+//  Copyright (c) 2013 RVC Student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MediaPlayer/MediaPlayer.h>
+
+@interface trailer3ViewController : UIViewController
+{
+    MPMoviePlayerController *player;
+}
+
+@end
